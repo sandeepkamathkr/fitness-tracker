03f92e476576535ca36ca84fc432e601e17f4b7c
Sandeep <sandeepkamathkr@gmail.com>
2020-07-28 21:39:59 +1000

    adding the training component with datatable and Training Services.

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,6 +1,7 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatDialog} from "@angular/material/dialog";
 import {StopTrainingComponent} from "./stop-training.component";
+import {TrainingService} from "../training.service";
 
 @Component({
   selector: 'app-current-training',
@@ -8,27 +9,30 @@ import {StopTrainingComponent} from "./stop-training.component";
   styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
-  @Output() stopTrainingEmitter: EventEmitter<any> = new EventEmitter<any>();
 
   progress: number = 0;
   timer: number;
   result: boolean = false;
 
-  constructor(private dialog: MatDialog) {
+  constructor(private dialog: MatDialog,
+              private trainingService: TrainingService) {
   }
 
   ngOnInit(): void {
     this.startOrResumeTimer();
   }
 
-  startOrResumeTimer(): void{
+  startOrResumeTimer(): void {
+    const step = this.trainingService.getRunningExercise().duration / 100 * 1000;
     this.timer = setInterval(() => {
       this.progress = this.progress + 5;
       if (this.progress >= 100) {
+        this.trainingService.completeExercise();
         clearInterval(this.timer);
       }
-    }, 1000);
+    }, step);
   }
+
   onStop(): void {
     clearInterval(this.timer);
     const dialogRef = this.dialog.open(StopTrainingComponent, {
@@ -38,8 +42,8 @@ export class CurrentTrainingComponent implements OnInit {
     })
     dialogRef.afterClosed().subscribe(result => {
       if (result) {
-        this.stopTrainingEmitter.emit();
-      }else{
+        this.trainingService.cancelExercise(this.progress);
+      } else {
         this.startOrResumeTimer();
       }
     })
