11df803cdc9ca471cb69f541896a4ae9a20eb2dc
Sandeep <sandeepkamathkr@gmail.com>
2020-07-28 13:27:57 +1000

    added Authentication service, Router connection to logout and login

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,20 +1,33 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, OnDestroy, OnInit, Output} from '@angular/core';
+import {AuthService} from "../../auth/auth.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.css']
 })
-export class SidenavListComponent implements OnInit {
-
+export class SidenavListComponent implements OnInit,OnDestroy {
   @Output() sideNavClose: EventEmitter<void> = new EventEmitter<void>();
+  isAuth: boolean = false;
+  authSubscription: Subscription;
 
-  constructor() { }
+  constructor(private authService:AuthService) { }
 
   ngOnInit(): void {
+    this.authSubscription = this.authService.authChange.subscribe(authStatus => this.isAuth = authStatus);
   }
 
   onClose(): void {
     this.sideNavClose.emit();
+  }
+
+  onLogout(){
+    this.onClose();
+    this.authService.logout();
+  }
+
+  ngOnDestroy() {
+    this.authSubscription.unsubscribe();
   }
 }
